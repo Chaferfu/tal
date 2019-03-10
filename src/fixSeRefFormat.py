6222fc71dcdd4dcbdd512bc88cf8c6f1c2d499be
Chaferfu <mathba@gmx.com>
2019-03-10 01:01:39 +0100

    bon ya un result pour stan se

--- src/fixSeRefFormat.py
+++ src/fixSeRefFormat.py
@@ -9,7 +9,7 @@ def peek(f, size = 1):
     return line
 
 
-with open(sys.argv[2], "w") as out:
+with open(sys.argv[2]+".temp", "w") as out:
 
     with open(sys.argv[1]) as file:
 
@@ -36,7 +36,17 @@ with open(sys.argv[2], "w") as out:
                         seType += c
                         c = file.read(1)
 
-                
+                    # print(seType)
+                    file.read(1)
+            else:
+                out.write(c)
 
-        
+with open(sys.argv[2], "w") as out:
 
+    with open(sys.argv[2]+".temp") as file:
+
+        for line in file:
+            if line == "\n": continue
+            out.write(line.replace(". ", "\n").replace("Mr\n", "Mr. ").replace("Ms\n","Ms. "))
+
+print("Wrote output at " + sys.argv[2])
